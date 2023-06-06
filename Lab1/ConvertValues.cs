369597f7363e214f308725a05f25278785678da9
dinethmin <chanudadinethmin@gmail.com>
2023-06-06 11:02:25 +0530

    Qustion1

---
+++ Lab1/ConvertValues.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    internal class ConvertValues
+    {
+        public double kilometerTOmeter(double km)
+        {
+            double m = 0;
+            m = km * 1000;
+            return m;
+            
+            
+        }
+    }
+}
---
+++ Lab1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the KM value: ");
+            double km = double.Parse(Console.ReadLine());
+            
+            
+            ConvertValues obj = new ConvertValues();
+
+            Console.WriteLine(km + "KM in m is,\n" + obj.kilometerTOmeter(km) + "m");
+
+            Console.ReadKey();
+        }
+    }
+}
