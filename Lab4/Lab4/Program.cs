0fef5a18be1a9ef5e9c9ddc54c2bb8c3d67704a6
dinethmin <chanudadinethmin@gmail.com>
2023-07-04 10:14:47 +0530

    Lab4

---
+++ Lab4/Lab4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enther the array size: ");
+            int size = int.Parse(Console.ReadLine());
+
+            Array objarray = new Array();
+            objarray.createarray(size);
+
+            Console.ReadKey();
+        }
+    }
+}
